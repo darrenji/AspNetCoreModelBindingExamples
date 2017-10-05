511ce7beb60c42bfdbd4ce01f35ff0548564a17b
darren <764190362@qq.com>
2017-10-05 17:01:40 +0800

    完成架构搭建

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreModelBindingExamples.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetCoreModelBindingExamples.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository;
+
+        public HomeController(IRepository repo)
+        {
+            repository = repo;
+        }
+        // GET: /<controller>/
+        public IActionResult Index(int id)
+        {
+            return View(repository[id]);
+        }
+    }
+}
